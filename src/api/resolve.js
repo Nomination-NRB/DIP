f2400e8d6aeb14b6e8c08cde7af97ddb6aaf673f
Nomination-NRB <1366213780@qq.com>
2022-05-28 23:08:46 +0800

    feature[complete api]

--- src/api/resolve.js
+++ src/api/resolve.js
@@ -41,7 +41,35 @@ export function rotate(data) {
   });
 }
 
-// 反转
+// 翻转
+export function reversal(data) {
+  return request({
+    url: "/reversal/",
+    method: "post",
+    data,
+  });
+}
+
+// 平移
+export function translate(data) {
+  return request({
+    url: "/translate/",
+    method: "post",
+    data,
+  });
+}
+
+// 对数变换
+export function logChange(data) {
+  return request({
+    url: "/logChange/",
+    method: "post",
+    data,
+  });
+}
+
+
+// 反色变换
 export function reverseChange(data) {
   return request({
     url: "/reverseChange/",
@@ -50,6 +78,32 @@ export function reverseChange(data) {
   });
 }
 
+// 幂次变换
+export function gammaChange(data) {
+  return request({
+    url: "/gammaChange/",
+    method: "post",
+    data,
+  });
+}
+
+// 直方图均衡化
+export function histogramToBalance(data) {
+  return request({
+    url: "/histogramToBalance/",
+    method: "post",
+    data,
+  });
+}
+
+// 直方图归一化
+export function histogramToOne(data) {
+  return request({
+    url: "/histogramToOne/",
+    method: "post",
+    data,
+  });
+}
 
 // 分段线性变换
 export function linearChange(data) {
@@ -60,4 +114,120 @@ export function linearChange(data) {
   });
 }
 
+// 对比度拉伸
+export function contrast(data) {
+  return request({
+    url: "/contrast/",
+    method: "post",
+    data,
+  });
+}
+
+// 椒盐噪声
+export function addSaltPepper(data) {
+  return request({
+    url: "/addSaltPepper/",
+    method: "post",
+    data,
+  });
+}
+
+// 高斯噪声
+export function addGaussian(data) {
+  return request({
+    url: "/addGaussian/",
+    method: "post",
+    data,
+  });
+}
+
+// Motion/Disk模糊操作
+export function motion(data) {
+  return request({
+    url: "/motion/",
+    method: "post",
+    data,
+  });
+}
+
+// 维纳滤波
+export function wiener(data) {
+  return request({
+    url: "/wiener/",
+    method: "post",
+    data,
+  });
+}
+
+// 平滑约束复原
+export function smooth(data) {
+  return request({
+    url: "/smooth/",
+    method: "post",
+    data,
+  });
+}
+
+// 自适应中值滤波
+export function selfMedian(data) {
+  return request({
+    url: "/selfMedian/",
+    method: "post",
+    data,
+  });
+}
+
+// 自适应均值滤波
+export function selfMean(data) {
+  return request({
+    url: "/selfMean/",
+    method: "post",
+    data,
+  });
+}
+
+// 平滑滤波（中值/均值）
+export function filter(data) {
+  return request({
+    url: "/filter/",
+    method: "post",
+    data,
+  });
+}
+
+// 锐化滤波
+export function sharpen(data) {
+  return request({
+    url: "/sharpen/",
+    method: "post",
+    data,
+  });
+}
+
+// 傅里叶变换
+export function fft(data) {
+  return request({
+    url: "/fft/",
+    method: "post",
+    data,
+  });
+}
+
+// 低通滤波
+export function lowFilter(data) {
+  return request({
+    url: "/lowFilter/",
+    method: "post",
+    data,
+  });
+}
+
+// 高通滤波
+export function highFilter(data) {
+  return request({
+    url: "/highFilter/",
+    method: "post",
+    data,
+  });
+}
 
